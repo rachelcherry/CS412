218d8830147f53fb4c1a1891c67d925a4745796b
rachelcherry <rcherry@bu.edu>
2024-11-17 14:33:43 -0500

    accidentally overwrote last project with new project

--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.ShowAllEntertainmentView.as_view(), name='home'),
+    path('', views.ShowAllEntertainmentView.as_view(), name='all_entertainment'),
     path('profiles', views.ShowAllPeopleView.as_view(), name='users'),
     path('profile/<int:pk>', views.ShowProfilePageView.as_view(), name='person'),
     path('entertainment/<int:pk>', views.ShowEntertainmentPageView.as_view(), name='entertainment'),
